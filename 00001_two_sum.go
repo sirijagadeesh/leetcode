4836e633eccc7a85518ab24f10d998ad43601f57
Jagadeesh Nalla <siri.jagadeesh@gmail.com>
2023-11-17 09:34:13 +0530

    first problem

---
+++ 00001_two_sum.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	val := make(map[int]int)
+
+	for i, v := range nums {
+		if j, ok := val[v]; ok {
+			return []int{j, i}
+		}
+		val[target-v] = i
+	}
+
+	return nil
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Golang Leetcode Solutions: Work in Progress
+
+|---|---|---|
+|No|Problem|Solution|
+|---|---|---|
+|00001|[Two Sum](./00001_two_sum.md)|[Go](./00001_two_sum.go)
